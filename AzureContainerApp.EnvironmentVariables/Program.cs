3b809b4b6804cd6edf73700799c505367bfcfdfb
Adolphous Navaratnam <470487+nshathish@users.noreply.github.com>
2025-07-15 08:32:12 +0100

    default route added to api

--- AzureContainerApp.EnvironmentVariables/Program.cs
+++ AzureContainerApp.EnvironmentVariables/Program.cs
@@ -16,6 +16,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => "Hello World!")
+    .WithName("GetHelloWorld");
+
 app.MapGet("/weatherforecast", () =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
