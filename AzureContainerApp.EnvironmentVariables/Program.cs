5cff4d8fcf8bf1299a65da5b5ed02797c067161e
nshathish <470487+nshathish@users.noreply.github.com>
2026-02-05 06:16:30 +0000

    refactor endpoints to their own files#

--- AzureContainerApp.EnvironmentVariables/Program.cs
+++ AzureContainerApp.EnvironmentVariables/Program.cs
@@ -1,3 +1,6 @@
+using AzureContainerApp.EnvironmentVariables.Endpoints;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -7,47 +10,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(opts =>
+    {
+        opts.DarkMode = false;
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/", () => "Hello World!")
-    .WithName("GetHelloWorld");
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.MapGet("/api/ship-theory", () =>
-    {
-        var shipTheoryUrl = builder.Configuration.GetValue<string>("ShipTheoryApiUrl");
-        if (string.IsNullOrEmpty(shipTheoryUrl))
-        {
-            return Results.BadRequest("ShipTheoryApiUrl is not configured.");
-        }
-
-        return Results.Ok(new { Url = shipTheoryUrl });
-    })
-    .WithName("GetShipTheoryUrl");
+app.MapHelloWorldEndpoint();
+app.MapWeatherForecastEndpoint();
+app.MapShipTheoryEndpoint();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
